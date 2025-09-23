c92749fe9dcb759af58d936542f9486dbf8baa7d
Lefrec <blackfox244@gmail.com>
2025-09-23 17:56:40 +0200

    new gallery

--- backend/backend.js
+++ backend/backend.js
@@ -2,24 +2,24 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
-export async function addSVG(name, code) {
-    try {
-        const SVG = await pb.collection('SVG').create({name, code});
-        return {
-            success: true,
-            message: "SVG ajouté à pocketbase"
-        }
-    } catch (error) {
-        return {
-            success : false,
-            message : "Problème avec le pocketbase"
-        }
-    }
-}
+// export async function addSVG(name, code) {
+//     try {
+//         const SVG = await pb.collection('SVG').create({name, code});
+//         return {
+//             success: true,
+//             message: "SVG ajouté à pocketbase"
+//         }
+//     } catch (error) {
+//         return {
+//             success : false,
+//             message : "Problème avec le pocketbase"
+//         }
+//     }
+// }
 
 export async function getAllSVG() {
     try {
-        const SVGs = await pb.collection('SVG').getFullList();
+        const SVGs = await pb.collection('SavedSVG').getFullList();
         return SVGs
     } catch (error) {
         return {
