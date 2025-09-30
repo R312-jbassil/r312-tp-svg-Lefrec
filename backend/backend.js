4c12b1be468b783ca222d6c30789ef13401ca8b0
Lefrec <blackfox244@gmail.com>
2025-09-30 16:14:46 +0200

    gallery dynamic routing

--- backend/backend.js
+++ backend/backend.js
@@ -2,25 +2,23 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
-// export async function addSVG(name, code) {
-//     try {
-//         const SVG = await pb.collection('SVG').create({name, code});
-//         return {
-//             success: true,
-//             message: "SVG ajouté à pocketbase"
-//         }
-//     } catch (error) {
-//         return {
-//             success : false,
-//             message : "Problème avec le pocketbase"
-//         }
-//     }
-// }
 
 export async function getAllSVG() {
     try {
         const SVGs = await pb.collection('SavedSVG').getFullList();
         return SVGs
+    } catch (error) {
+        return {
+            success : false,
+            message : "Problème avec le pocketbase"
+        }
+    }
+}
+
+export async function getSVG(id) {
+    try {
+        const SVG = await pb.collection('SavedSVG').getOne(id);
+        return SVG
     } catch (error) {
         return {
             success : false,
---
+++ src/i18n/ui.js
@@ -0,0 +1,58 @@
+export const ui = {
+  en: {
+    nav: {
+      home: 'Home',
+      generator: 'Generator',
+      gallery: 'Gallery',
+      language: 'Language',
+      free: 'FREE',
+    },
+    index: {
+        title: 'Welcome to SVG Generator',
+        description: 'Create and render SVGs from prompts.',
+        button: 'Go to SVG Generator',
+        library: 'Go to Library'
+    },
+    generator: {
+        title: 'SVG Generator',
+        contentPlaceholder: 'SVG content will be displayed here',
+        codePlaceholder: 'SVG code will be displayed here',
+        promptLabel: 'Enter your prompt:',
+        generateButton: 'Generate SVG',
+        editButton: 'Edit',
+        saveButton: 'Save',
+    },
+    gallery: {
+        title: 'SVG Gallery',
+        viewDetails: 'View Details',
+    }
+  },
+  fr: {
+    nav: {
+      home: 'Accueil',
+      generator: 'Générateur',
+      gallery: 'Galerie',
+      language: 'Langue',
+      free: 'GRATUIT',
+    },
+    index: {
+        title: 'Bienvenue sur le générateur SVG',
+        description: 'Créez et affichez des SVG à partir d\'invites.',
+        button: 'Aller au générateur SVG',
+        library: 'Aller à la galerie'
+    },
+    generator: {
+        title: 'Générateur SVG',
+        contentPlaceholder: 'Le contenu SVG sera affiché ici',
+        codePlaceholder: 'Le code SVG sera affiché ici',
+        promptLabel: 'Entrez votre invite :',
+        generateButton: 'Générer le SVG',
+        editButton: 'Éditer',
+        saveButton: 'Sauvegarder',
+    },
+    gallery: {
+        title: 'Galerie SVG',
+        viewDetails: 'Voir les détails',
+    }
+  },
+}
