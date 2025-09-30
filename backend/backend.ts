120a7bd865b283a9dde90e0230a8b1d6c79036fe
Lefrec <blackfox244@gmail.com>
2025-09-30 17:39:11 +0200

    end of TP4

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,11 +1,12 @@
 import PocketBase from 'pocketbase';
+import { Collections, type SavedSVGRecord } from "../src/utils/pocketbase-types";
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 
 export async function getAllSVG() {
     try {
-        const SVGs = await pb.collection('SavedSVG').getFullList();
+        const SVGs = await pb.collection(Collections.SavedSVG).getFullList();
         return SVGs
     } catch (error) {
         return {
@@ -17,7 +18,7 @@ export async function getAllSVG() {
 
 export async function getSVG(id) {
     try {
-        const SVG = await pb.collection('SavedSVG').getOne(id);
+        const SVG = await pb.collection(Collections.SavedSVG).getOne(id);
         return SVG
     } catch (error) {
         return {
